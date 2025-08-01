895c168799ddf3435711f217c86707fdc5d9a2df
BlVladimir <vblinov555@gmail.com>
2025-08-01 12:39:24 +0300

    Refactor tile selection and usage logic:
    - Introduced `UsingElementController` for managing selected and active tiles.
    - Updated `TilesController` to delegate tile selection, unselection, and usage to `UsingElementController`.
    - Simplified tile activation process and improved shop interaction handling.
    - Removed redundant selection and activation logic in `TilesController`.

--- scripts/arrays_handlers/arrays_controllers/maps/tiles_controller.py
+++ scripts/arrays_handlers/arrays_controllers/maps/tiles_controller.py
@@ -1,10 +1,11 @@
-from logging import warning
+from logging import warning, error
 
 from panda3d.core import PandaNode
 
 from scripts.arrays_handlers.arrays_controllers.maps.creating_map.map_tiles_builder import MapTilesBuilder
 from scripts.arrays_handlers.arrays_controllers.maps.maps_config import MapsConfig
 from scripts.arrays_handlers.arrays_controllers.maps.tile import Tile
+from scripts.arrays_handlers.parts_handler.using_element_controller import UsingElementController
 from scripts.interface.i_context import IContext
 from scripts.main_classes.settings import Settings
 from scripts.sprite.rect import Rect3D
@@ -15,42 +16,31 @@ class TilesController:
     """Содержит группу всех тайлов"""
     def __init__(self, maps_config:MapsConfig, maps_node:PandaNode, loader, context:IContext):
         self.__map_tiles_builder = MapTilesBuilder(maps_config, maps_node, loader)
-        self._selected_tile_sprite = None
-        self._using_tile_sprite = None
+        self._using_tile = UsingElementController(using_action=self.using_action, unused_action=lambda cont=context: cont.buttons_controller.close_shop())
         self.__context = context
 
     def create_map_tiles(self, level, settings:Settings):
         """Создает тайлы для карты карту"""
         self.__map_tiles_builder.create_map_tiles(level, settings)
 
-    def select_tile(self, tile_sprite:Sprite3D):
-        """Выделяет тайл"""
-        if self._selected_tile_sprite != tile_sprite:
-            if self._selected_tile_sprite is not None and (self._selected_tile_sprite != self._using_tile_sprite):
-                self._selected_tile_sprite.is_using = False
-            self._selected_tile_sprite = tile_sprite
-            tile_sprite.is_using = True
-
-    def unselect_tile(self):
-        """Убирает выделение"""
-        if self._selected_tile_sprite != self._using_tile_sprite:
-            self._selected_tile_sprite.is_using = False
-        self._selected_tile_sprite = None
-
-    def using_tile(self):
-        """Назначает тайл активным"""
-        if not self._using_tile_sprite is None:
-            self._using_tile_sprite.is_using = False
-            self.__context.buttons_controller.close_shop()
-        if not self._selected_tile_sprite is None:
-            self._using_tile_sprite = self._selected_tile_sprite
-            self._using_tile_sprite.is_using = True
-            if self._using_tile_sprite.external_object.effect != 'road' and not self._using_tile_sprite.main_node.find('tower'):
-                self.__context.buttons_controller.open_shop()
-
-
     def reset_tiles(self):
         self.__map_tiles_builder.reset_map()
+
+    def using_action(self):
+        if self._using_tile.using_tile_sprite.external_object.effect != 'road' and not self._using_tile.using_tile_sprite.main_node.find('tower'):
+            self.__context.buttons_controller.open_shop()
+
+    def select_tile(self, tile_sprite:Sprite3D)->None:
+        """Выделяет тайл"""
+        self._using_tile.select_sprite(tile_sprite)
+
+    def unselect_tile(self)->None:
+        """Убирает выделение"""
+        self._using_tile.unselect_sprite()
+
+    def using_tile(self)->None:
+        """Назначает тайл активным"""
+        self._using_tile.using_sprite()
 
     @property
     def first_tile_rect(self)->Rect3D:
@@ -58,7 +48,7 @@ class TilesController:
 
     @property
     def selected_tile(self)->Tile:
-        return self._using_tile_sprite.external_object
+        return self._using_tile.using_tile_sprite.external_object
 
     @property
     def track(self):
---
+++ scripts/arrays_handlers/parts_handler/using_element_controller.py
@@ -0,0 +1,40 @@
+from typing import Callable
+
+from scripts.sprite.sprite3D import Sprite3D
+
+
+class UsingElementController:
+    def __init__(self, using_action:Callable=lambda:None, unused_action:Callable=lambda:None):
+        self._selected_sprite = None
+        self._using_sprite = None
+        self.__using_action = using_action
+        self.__unused_action = unused_action
+
+    def select_sprite(self, tile_sprite: Sprite3D) -> None:
+        """Выделяет тайл"""
+        if self._selected_sprite != tile_sprite:
+            if self._selected_sprite is not None and (self._selected_sprite != self._using_sprite):
+                self._selected_sprite.is_using = False
+            self._selected_sprite = tile_sprite
+            self._selected_sprite.is_using = True
+
+    def unselect_sprite(self)->None:
+        """Убирает выделение"""
+        if self._selected_sprite != self._using_sprite:
+            self._selected_sprite.is_using = False
+        self._selected_sprite = None
+
+    def using_sprite(self)->None:
+        """Назначает тайл активным"""
+        if not self._using_sprite is None:
+            self._using_sprite.is_using = False
+            self.__unused_action()
+            self._using_sprite = None
+        if not self._selected_sprite is None:
+            self._using_sprite = self._selected_sprite
+            self._using_sprite.is_using = True
+            self.__using_action()
+
+    @property
+    def using_tile_sprite(self):
+        return self._using_sprite
